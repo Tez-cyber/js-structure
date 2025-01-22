484ca476a3ac76ab13ab72daa6c3bf8edc9563a1
Salami Azeez <salamitopeazeez@gmail.com>
2025-01-22 01:28:36 +0100

    Added checkBracket folder

--- checkBracket.js
+++ checkBracket.js
@@ -28,3 +28,39 @@ Input: s = "{[]}"
 Output: valid
 
 */
+
+const isValid = (passedString) => {
+    let stack = [];
+    let bracketPairs = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+
+    // For loop to check each values passed in the function call
+    for (let eachChar of passedString) {
+        // check is char is an object key in bracketPair
+        if (bracketPairs[eachChar]) {
+            /** Check the length of stack
+             * Compare the last value of stack to char
+             */
+            if(stack.length > 0 && stack[stack.length - 1] === bracketPairs[eachChar]) {
+                stack.pop()
+            }
+        } else {
+            /**
+             * if its not an object key in bracketPairs,
+             *  push into stack array
+             */
+            stack.push(eachChar)
+        }
+    }
+
+    return stack.length === 0 ? "valid" : "invalid"
+}
+
+console.log(isValid("()")); 
+console.log(isValid("()[]{}")); 
+console.log(isValid("(]"));     
+console.log(isValid("([)]"));   
+console.log(isValid("{[]}"));    
