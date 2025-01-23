b13066afd5a402aab095714c4e6955c7bc767ae6
Salami Azeez <salamitopeazeez@gmail.com>
2025-01-23 13:24:29 +0100

    Added first apporach to solving question

--- checkMultipleValues/checkMultipleValues.js
+++ checkMultipleValues/checkMultipleValues.js
@@ -1,2 +1,11 @@
 
+// =========== First Approach
+const stringCount = (str) => {
+    const count = str.split("");
+
+    const checkDuplicate = new Set(count);
+    return checkDuplicate.size;
+}
+
+
 
