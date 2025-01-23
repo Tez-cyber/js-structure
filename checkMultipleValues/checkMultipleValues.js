6b14c90f39742b7dc019a81a0022ff76e51a2158
Salami Azeez <salamitopeazeez@gmail.com>
2025-01-23 13:46:42 +0100

    Added 2 more approaches to solving question

--- checkMultipleValues/checkMultipleValues.js
+++ checkMultipleValues/checkMultipleValues.js
@@ -8,4 +8,32 @@ const stringCount = (str) => {
 }
 
 
+// ============= Second Approach
+const imperativeCount = (str) => {
+    const uniqueArr = [];
+    const count = str.split("");
+
+    for(let i = 0; i < count.length; i++) {
+        if(uniqueArr.indexOf(str[i]) === -1) {
+            uniqueArr.push(str[i]);
+        }
+    }
+
+    return uniqueArr;
+}
+
+// ================== Third Approach
+const checkDuplicate = (str) => {
+    const count = str.split("");
+    const uniqueArr = [];
+
+    for(let i = 0; i < count.length; i++) {
+        if(!uniqueArr.includes(str[i])) {
+            uniqueArr.push(str[i])
+        }
+    }
+
+    return uniqueArr
+}
+console.log(checkDuplicate("ababaccdddaaabbbadddaddds"))
 
